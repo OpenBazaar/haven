f486f3332480fe0a03e1fd01cde24549e9d7b82c
heyradcode <betterthanbestest@gmail.com>
2020-08-05 22:49:45 -0400

    feat: havenBuildConfigFiles setup

---
+++ havenBuildConfigFiles/copy_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ./GoogleService-Info.plist ../ios
+cp ./google-services.json ../android/app
+
+cp ./AppCenter-Config.plist ../ios/NativeBazaar
+cp ./appcenter-config.json ../android/app/src/main/assets
